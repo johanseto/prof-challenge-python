b11b1c79b191acddd04c154eed8c902c4d8d3540
johan.castiblanco <johan.castiblanco@pevaar.com>
2021-08-01 15:49:54 -0500

    incredible sets way--working with sets
    deduping a list with sets could be awesome.
    
    clasiccal way
     ****************************************************************************************************
    Pasaron 74.542991 segundos
    sets way
     ****************************************************************************************************
    Pasaron 0.006981 segundos

---
+++ sets_dedupe.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+def execution_time(func):
+    def wrapper(*args, **kwargs):
+        initial_time = datetime.now()
+        result = func(*args, **kwargs)
+        final_time = datetime.now()
+        time_elapsed = final_time - initial_time
+        print(f'Pasaron {time_elapsed.total_seconds()} segundos')
+        return result
+    return wrapper
+
+
+@execution_time
+def remove_duplicates(some_list):
+    without_duplicates = []
+    for element in some_list:
+        if element not in without_duplicates:
+            without_duplicates.append(element)
+    return without_duplicates
+
+@execution_time
+def remove_duplicates_with_sets(some_list):
+    return list(set(some_list))
+
+def run():
+    random_list = [1, 2, 2, 2, 3, "Platzi", "Platzi", True, 4.6, False]
+    random_list.extend([x for x in range(100000)])
+    print("clasiccal way \n",100*"*")
+    remove_duplicates(random_list)
+    print("sets way\n",100*"*")
+    remove_duplicates_with_sets(random_list)
+
+
+if __name__ == '__main__':
+    run()
